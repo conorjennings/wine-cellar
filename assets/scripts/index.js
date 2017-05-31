a8c6b856b2e20f442aed5b85771cc35cd43c2cf5
“conorjennings” <conorjennings@gmail.com>
2017-05-31 14:10:07 -0400

    Completed version of video loader.
    Used a @require statement in index.js to load the file before calling
    it in a DIV tag also in index.js. Worked locally. Now testing deployment.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,15 +4,18 @@ const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/auth-events.js')
 const uxEvents = require('./uxShowHideClear')
-const grapes = require('../../images/grapes.jpg')
+// const grapes = require('../../images/grapes.jpg')
+// const html = `<img src=${grapes} />`
+const winePourVideo = require('../../video/redWineSlowPour.mp4')
 
-const html = `<img src=${grapes} />`
+const html = `<video loop='1' autoplay class='responsive-video'> <source src=${winePourVideo} type='video/mp4'> </video>`
 
 $(() => {
   setAPIOrigin(location, config)
   authEvents.authHandlers()
   uxEvents.uxHandlers()
-  $('#grape-picture').append(html)
+  // $('#grape-picture').append(html)
+  $('#wine-video').append(html)
 })
 
 // use require with a reference to bundle the file and use it in this file
